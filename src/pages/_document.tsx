2ed2461f5f6779e20c6dbde0bfb41705f1ba09d8
Kjell-eik <stefanbringaker@gmail.com>
2026-02-17 16:53:21 +0200

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,6 @@ export default function Document() {
         <Html>
             <Head />
             <body>
-                {/* Hidden form for Netlify Forms detection */}
-                <form name="sign-up-form" netlify netlify-honeypot="bot-field" hidden>
-                    <input type="text" name="firstName" />
-                    <input type="text" name="lastName" />
-                    <input type="email" name="email" />
-                    <input type="text" name="address" />
-                </form>
                 <Main />
                 <NextScript />
             </body>
